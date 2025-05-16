c11ef305d579e178d92d07b09bedbd4766787b24
Cyan Kneelawk <kneelawk@gmail.com>
2025-05-15 18:41:14 -0700

    Do more surface stuff

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ use anyhow::Context;
 use cfg_if::cfg_if;
 use std::path::PathBuf;
 use std::sync::Arc;
-use wgpu::{Adapter, Instance, Surface};
+use tokio::runtime;
+use tokio::runtime::Runtime;
+use wgpu::{Adapter, Device, Instance, Queue, Surface};
 use winit::application::ApplicationHandler;
 use winit::dpi::PhysicalSize;
 use winit::event::WindowEvent;
@@ -45,7 +47,7 @@ pub fn run_impl(args: StartupArgs) -> anyhow::Result<()> {
 
     info!("Initializing...");
 
-    let mut app = App::new(args);
+    let mut app = App::new(args).context("Creating app")?;
 
     let event_loop = EventLoop::new().context("Initializing event loop")?;
 
@@ -60,6 +62,7 @@ pub fn run_impl(args: StartupArgs) -> anyhow::Result<()> {
 
 struct App {
     args: StartupArgs,
+    runtime: Runtime,
     instance: Instance,
     surface: Option<AppSurface>,
 }
@@ -67,25 +70,43 @@ struct App {
 struct AppSurface {
     window: Arc<Window>,
     surface: Surface<'static>,
-    adapter: Adapter,
+    device: Device,
+    queue: Queue,
+    config: wgpu::SurfaceConfiguration,
 }
 
 impl App {
-    fn new(cli: StartupArgs) -> Self {
+    fn new(args: StartupArgs) -> anyhow::Result<Self> {
+        let runtime;
+        #[cfg(target_arch = "wasm32")]
+        {
+            runtime = runtime::Builder::new_current_thread()
+                .build()
+                .context("Creating tokio runtime")?;
+        }
+
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            runtime = Runtime::new()?;
+        }
+
         let instance = Instance::new(&wgpu::InstanceDescriptor {
-            #[cfg(not(target_arch = "wasm32"))]
+            #[cfg(not(all(target_arch = "wasm32", feature = "webgl")))]
             backends: wgpu::Backends::PRIMARY,
-            #[cfg(target_arch = "wasm32")]
+            #[cfg(all(target_arch = "wasm32", feature = "webgl"))]
             backends: wgpu::Backends::GL,
             ..Default::default()
         });
 
-        App {
-            args: cli,
+        Ok(App {
+            args,
+            runtime,
             instance,
             surface: None,
-        }
+        })
     }
+    
+    // TODO: resize
 }
 
 impl ApplicationHandler for App {
@@ -100,6 +121,8 @@ impl ApplicationHandler for App {
                 .expect("Creating window"),
         );
 
+        let size = window.inner_size();
+
         #[cfg(target_arch = "wasm32")]
         {
             use winit::platform::web::WindowExtWebSys;
@@ -107,7 +130,9 @@ impl ApplicationHandler for App {
                 .and_then(|win| win.document())
                 .and_then(|doc| {
                     let dst = doc.get_element_by_id("viewer3d-wasm")?;
-                    let canvas = web_sys::Element::from(window.canvas());
+                    let canvas = web_sys::Element::from(
+                        window.canvas().expect("Unable to get window canvas"),
+                    );
                     dst.append_child(&canvas).ok()?;
                     Some(())
                 })
@@ -119,18 +144,57 @@ impl ApplicationHandler for App {
             .create_surface(window.clone())
             .expect("Error creating surface");
 
-        let adapter = self.instance.request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: Default::default(),
-            force_fallback_adapter: false,
-            compatible_surface: Some(&surface),
+        let adapter = self
+            .runtime
+            .block_on(self.instance.request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            }))
+            .expect("Error creating adapter");
+
+        let (device, queue) = self
+            .runtime
+            .block_on(adapter.request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: if cfg!(all(target_arch = "wasm32", feature = "webgl")) {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            }))
+            .expect("Error requesting device");
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            desired_maximum_frame_latency: 2,
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        self.surface = Some(AppSurface {
+            window,
+            surface,
+            device,
+            queue,
+            config,
         });
-
-        self.surface = Some(AppSurface { window, surface, adapter });
-    }
-
-    fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
-        // drop surface
-        self.surface = None;
     }
 
     fn window_event(
@@ -147,5 +211,10 @@ impl ApplicationHandler for App {
             WindowEvent::RedrawRequested => {}
             _ => {}
         }
+    }
+
+    fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
+        // drop surface
+        self.surface = None;
     }
 }
